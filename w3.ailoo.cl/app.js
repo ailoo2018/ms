bb266cc95e76413efb3b8aec97de4916fb7c7cc5
ailoo2018 <jcfuentes@ailoo.cl>
2026-01-03 11:00:35 -0300

    asdf:wq

--- w3.ailoo.cl/app.js
+++ w3.ailoo.cl/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const logger = require("@ailoo/shared-libs/logger")
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
+  logger.info('App started on port: ' + PORT);
   res.send('Hello World! w3.ailoo.cl v3.3.3.3.5 ');
 });
 
