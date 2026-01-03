9ec5b2901a8e275e48670e4f0c937f321905aaca
ailoo2018 <jcfuentes@ailoo.cl>
2026-01-03 11:45:41 -0300

    3.5

--- w3.ailoo.cl/app.js
+++ w3.ailoo.cl/app.js
@@ -1,13 +1,46 @@
+require("@ailoo/shared-libs/config")
+
 const express = require('express');
 const logger = require("@ailoo/shared-libs/logger")
-const app = express();
-const PORT = 3000;
 
-app.get('/', (req, res) => {
-  logger.info('App started on port: ' + PORT);
-  res.send('Hello World! w3.ailoo.cl v3.3.3.3.5 ' + logger);
+const {findWidget} = require("./db/webcontent");
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+app.use((err, req, res, next) => {
+  logger.error("Error in express handler: " + err);
+  if(err.stack != null)
+    logger.error(err.stack);
+  res.status(500);
+  if (!res.headersSent) {
+    res.json({
+      error: err.message,
+      stack: err.stack,
+      env: process.env.NODE_ENV});
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.get('/', (req, res) => {
+  res.send('Hello World! DB_HOST ' + process.env.DB_HOST);
+});
+
+
+app.get('/:domainId/wcc/:id', async (req, res, next) => {
+
+  try{
+    const id = parseInt(req.params.id);
+    const domainId = parseInt(req.params.domainId);
+
+    const wcc = await findWidget(id, domainId);
+
+    res.json(wcc)
+  }catch(err){
+    next(err)
+  }
+
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`Server is running on http://localhost:${app.get("port")}`);
 });
---
+++ w3.ailoo.cl/db/index.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2/promise');
+
+
+// Create a MySQL connection pool
+console.log("about to create pool");
+const pool = mysql.createPool({
+  host: process.env.DB_HOST, // 'ailoomysql.mysql.database.azure.com',
+  user: process.env.DB_USER, // 'motomundi',
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  multipleStatements: true,
+  waitForConnections: true,
+  connectTimeout: 60000,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+});
+
+
+module.exports.pool = pool;
