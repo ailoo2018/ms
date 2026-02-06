4dfcd62b37fb914906f09faba6e016ee06497779
ailoo2018 <jcfuentes@ailoo.cl>
2026-02-06 16:52:44 -0300

    3.5

--- w3.ailoo.cl/maint.js
+++ w3.ailoo.cl/maint.js
@@ -4,23 +4,7 @@ const {db} = require("./db/drizzle")
 
 
 
-
-
-// Use Prisma for type-safe SaleOrder operations
-async function prismaQuery() {
-  const result = await db.query.saleOrder.findFirst({
-    where: (saleOrder, { eq }) => eq(saleOrder.id, 123),
-    // This automatically joins the items table defined in our relations
-    with: {
-      items: true,
-    },
-  });
-
-
-}
-
-
 ( async () => {
-  await prismaQuery()
+
 
 })()
