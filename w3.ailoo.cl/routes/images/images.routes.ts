7aec7222b7ae579038d67d8826945369deec38a9
ailoo2018 <jcfuentes@ailoo.cl>
2026-03-17 20:00:52 -0300

    3.5

--- w3.ailoo.cl/routes/images/images.routes.ts
+++ w3.ailoo.cl/routes/images/images.routes.ts
@@ -3,6 +3,7 @@ import {productsClient} from "../../clients/productsClient.js";
 
  // Create a router instead of using 'app'
 import path from "path";
+import {pool} from "../../connections/mysql.js";
 
 const router = Router();
 
@@ -23,19 +24,57 @@ function extractGuid(input: string) {
 router.post("/:domainId/images/sizes", async (req : any, res : any, next : any) => {
     try {
         const domainId = parseInt(req.params.domainId);
-        const imageId = extractGuid(req.body.imageId);
+        const imageId = req.body.imageId ? extractGuid(req.body.imageId) : null;
+        const productId = parseInt(req.body.productId);
         const maintainAspectRatio = req.body.maintainAspectRatio || false;
         const sizes = req.body.sizes || [150, 300, 600, 800];
 
-        const response = await productsClient.createImageSizes(imageId, sizes, maintainAspectRatio, domainId)
+        if(productId > 0){
+            const imagesIds = await productImagesIds(productId);
 
-        return res.json(response);
+            const arrResp = []
+            for(const imgId of imagesIds){
+                const response = await productsClient.createImageSizes(imgId, sizes, maintainAspectRatio, domainId)
+                arrResp.push(response)
+            }
+
+            return res.json(arrResp)
+        }else{
+            const response = await productsClient.createImageSizes(imageId, sizes, maintainAspectRatio, domainId)
+            return res.json(response);
+        }
+
+
+
+
     } catch (e) {
         console.error(`Error calling /:domainId/images/sizes : body is ${JSON.stringify(req.body)}`)
         next(e)
     }
 
 })
+
+
+async function productImagesIds(productId: number){
+    const connection = await pool.getConnection();
+
+    try {
+        const [rows] = await connection.execute(
+            `
+                select * from productimage where ProductId = ?
+            `, [productId]) as any;
+
+
+        return rows.map(r => {
+            return r.Image
+        });
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await connection.release();
+    }
+
+}
 
 
 export default router;
